a0743897e07050d9d521a3f6afb1033e113c125d
Hernan Albornoz <calbornoz@frsf.utn.edu.ar>
2021-08-30 14:47:14 -0300

    Initial commit

---
+++ src/ms-pagos/ms-pagos/src/test/java/com/tpdan/mspagos/MsPagosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpdan.mspagos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPagosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/ms-pedidos/ms-pedidos/src/test/java/com/tpdan/mspedidos/MsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpdan.mspedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
